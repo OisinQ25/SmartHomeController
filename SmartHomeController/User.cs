b7a023f921ebfdfd946bf661e9ac70832025b0b7
Oisin Quinn <Quinn-O22@ulster.ac.uk>
2025-11-10 15:40:44 +0000

    Added Super Subsclass in smart Thermostat class

--- SmartHomeController/User.cs
+++ SmartHomeController/User.cs
@@ -8,5 +8,34 @@ namespace SmartHomeController
 {
     internal class User
     {
+        //Private Fields
+        private int userId;
+        private string username;
+        private string password;
+        private string contactinfo;
+        private bool isLoggedIn;
+
+        //Public properties
+        public int UserId {  
+            get { return userId; } 
+            set { userId = value; } 
+        }   
+        public string Username {  
+            get { return username; } 
+            set { username = value; } 
+        }
+        public string Password { 
+            get { return password; } 
+            set { password = value; } 
+        }
+        public string Contactinfo {
+            get { return contactinfo; } 
+            set { contactinfo = value; }  
+        }
+        public bool IsLoggedIn {
+            get { return isLoggedIn; } 
+            set { isLoggedIn = value; }
+        }
     }
+
 }
