78377a9824877dbadb5e2d5f1b17ca25e4b83143
Oisin Quinn <Quinn-O22@ulster.ac.uk>
2025-11-10 15:55:37 +0000

    user class created

--- SmartHomeController/User.cs
+++ SmartHomeController/User.cs
@@ -16,27 +16,51 @@ namespace SmartHomeController
         private bool isLoggedIn;
 
         //Public properties
-        public int UserId {  
-            get { return userId; } 
-            set { userId = value; } 
-        }   
-        public string Username {  
-            get { return username; } 
-            set { username = value; } 
+        public int UserId {
+            get { return userId; }
+            set { userId = value; }
         }
-        public string Password { 
-            get { return password; } 
-            set { password = value; } 
+        public string Username {
+            get { return username; }
+            set { username = value; }
+        }
+        public string Password {
+            get { return password; }
+            set { password = value; }
         }
         public string Contactinfo {
-            get { return contactinfo; } 
-            set { contactinfo = value; }  
+            get { return contactinfo; }
+            set { contactinfo = value; }
         }
         public bool IsLoggedIn {
-            get { return isLoggedIn; } 
+            get { return isLoggedIn; }
             set { isLoggedIn = value; }
         }
 
+        public bool Login(string userName, string password)
+        {
+            if (userName == userName && Password == password)
+            {
+                isLoggedIn = true;
+                Console.WriteLine("Login Successful!");
+            }
+            else {
+                IsLoggedIn = false;
+                Console.WriteLine($"Login failed. Username {username} or password is incorrect");
+            }
+            return isLoggedIn;
+        }
+        public void Logout()
+        {
+            if (IsLoggedIn)
+            {
+                IsLoggedIn = false;
+                Console.WriteLine("User is logged in successfully!");
+            }
+            else
+            {
+                Console.WriteLine("User is not logged in!");
+            }
+        }
     }
-
 }
